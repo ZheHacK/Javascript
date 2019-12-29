a662cc51795ddca546db8ad455d8a44cf84a7339
ZheHacK <54292567+ZheHacK@users.noreply.github.com>
2019-12-29 11:56:55 -0800

    Add files via upload

---
+++ standalone.json.js
@@ -0,0 +1,38 @@
+{
+  "name": "Nias Blog",
+  "short_name": "NiasBlog",
+  "start_url": "https://kepnis.blogspot.com/",
+  "display": "standalone",
+  "background_color": "#fff",
+  "theme_color": "#C51162",
+  "description": "Blog yang ditampilkan hanya text saja, tetapi di postingan bisa ditambah image dan video Youtube.",
+  "icons": [{
+    "src": "https://1.bp.blogspot.com/-f9Yr040UPww/XAudRlNfTqI/AAAAAAAAAfY/DGtFNovgnpQNzq_TpQzqTybiMjzFo-BHQCPcBGAYYCw/s1600/apple-icon-120x120.png",
+    "sizes": "48x48",
+    "type": "image/png"
+  }, {
+    "src": "https://1.bp.blogspot.com/-f9Yr040UPww/XAudRlNfTqI/AAAAAAAAAfY/DGtFNovgnpQNzq_TpQzqTybiMjzFo-BHQCPcBGAYYCw/s1600/apple-icon-120x120.png",
+    "sizes": "72x72",
+    "type": "image/png"
+  }, {
+    "src": "https://1.bp.blogspot.com/-f9Yr040UPww/XAudRlNfTqI/AAAAAAAAAfY/DGtFNovgnpQNzq_TpQzqTybiMjzFo-BHQCPcBGAYYCw/s1600/apple-icon-120x120.png",
+    "sizes": "96x96",
+    "type": "image/png"
+  }, {
+    "src": "https://1.bp.blogspot.com/-beDjtAu2OBM/XAudmSans0I/AAAAAAAAAfg/QqbHhTzU8y0sB0uxnRzd_UhfmbvvfU3nACPcBGAYYCw/s1600/apple-icon-152x152.png",
+    "sizes": "152x152",
+    "type": "image/png"
+  }, {
+    "src": "https://1.bp.blogspot.com/-36ACNFCV8Z0/XAud6yJPeaI/AAAAAAAAAfo/KP72txi8TgU5-uq-fFAYNjtGdef8_1KCACPcBGAYYCw/s1600/apple-icon-180x180.png",
+    "sizes": "180x180",
+    "type": "image/png"
+  }, {
+    "src": "https://1.bp.blogspot.com/-6ThiedlYYVc/XAueVrreNgI/AAAAAAAAAfw/Kyl7sJZe7f4HpkhR79D6_H6gGfoIr3RtQCPcBGAYYCw/s1600/android-icon-192x192.png",
+    "sizes": "192x192",
+    "type": "image/png"
+  }, {
+    "src": "https://1.bp.blogspot.com/-pjyohhCFydA/XGMbEJFRswI/AAAAAAAAAwU/TpEz3LX5U9Mv4x54IBmdTdZp34cO5EBzgCPcBGAYYCw/s1600/Logo.png",
+    "sizes": "512x512",
+    "type": "image/png"
+  }]
+}
